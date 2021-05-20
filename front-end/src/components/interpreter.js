804070c1f1ba8a75161548bbcc05722153168483
JillKlatt <klatt.jill@gmail.com>
2021-05-20 17:27:19 -0400

    Add Interpreter object with constructor

---
+++ front-end/src/components/interpreter.js
@@ -0,0 +1,11 @@
+class Interpreter {
+    constructor({id, name, email, phone, notes, cityId, languageId}){
+        this.id = id
+        this.name = name
+        this.email = email
+        this.phone = phone
+        this.notes = notes
+        this.cityId = cityId
+        this.languageId = languageId
+    }
+}

a72e0515603fe9168ff041024725a9ecd92a60d0
JillKlatt <klatt.jill@gmail.com>
2021-05-21 16:55:49 -0400

    Add delete interpreter and listen for delete functions, comment out

--- front-end/src/components/interpreterAdapter.js
+++ front-end/src/components/interpreterAdapter.js
@@ -11,13 +11,24 @@ class InterpreterAdapter {
     .then(resp => resp.json())
     .then(data => {
         data.forEach(function(interpreter){
-            interpreterContainer.innerHTML += `<li>${interpreter.name} -${interpreter.language.name}, in ${interpreter.city.name}</li>`
-            console.log(interpreter)
+            interpreterContainer.innerHTML += `<li data-id="${interpreter.id}">${interpreter.name} -${interpreter.language.name}, in ${interpreter.city.name}</li><button data-action='delete'>X</button>`
+            // console.log(interpreter)
         })
     })
     // .then(data => console.log(data))
     .catch(error => console.error(error))
 }
 
+    // listenforDelete() {
+    //     interpreterContainer.addEventListener('click', handleDelete)
+    // }
+
+    // handleDelete(e) {
+    //     if (e.target.dataset.action == 'delete'){
+    //         console.log("DELETE SOMETHING")
+    //     } else {
+    //     console.log("nope")
+    //     }
+    // }
 }
 

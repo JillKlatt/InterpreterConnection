4604119cb21b54f8e79fe8f003e0af81109a8992
JillKlatt <klatt.jill@gmail.com>
2021-05-21 16:57:52 -0400

    Add Addint to dom function in int class

--- front-end/src/components/interpreter.js
+++ front-end/src/components/interpreter.js
@@ -1,11 +1,18 @@
 class Interpreter {
-    constructor({id, name, email, phone, notes, cityId, languageId}){
+    constructor({id, name, email, phone, notes, city_id, language_id}){
         this.id = id
         this.name = name
         this.email = email
         this.phone = phone
         this.notes = notes
-        this.cityId = cityId
-        this.languageId = languageId
+        this.cityId = city_id
+        this.languageId = language_id
+    }
+
+
+    addIntToDom() {
+        // debugger
+        const interpreterContainer = document.getElementById('interpreters-container');
+        interpreterContainer.innerHTML += `<li>${this.name}</li>`// -${this.language}, in ${this.city.name}</li>`
     }
 }

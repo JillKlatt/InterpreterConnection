b050ac0cc2b7b4401ca4da4a3cfba69779e65de1
JillKlatt <klatt.jill@gmail.com>
2021-05-21 17:35:27 -0400

    moved addcreateform to form class, fixed delete from dom issue

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,46 +1,47 @@
 const interpreterAdapter = new InterpreterAdapter("http://localhost:3000")
 const cityAdapter = new CityAdapter("http://localhost:3000")
 const languageAdapter = new LanguageAdapter("http://localhost:3000")
+const interpreterForm = new InterpreterForm
 
 document.addEventListener("DOMContentLoaded", () => {
     interpreterAdapter.getInterpreters();
     cityAdapter.getCities();
     languageAdapter.getLanguages();
-    showCreateForm();
+    interpreterForm.addCreateForm();
     listenforDelete();
 })
 
-function addCreateForm() {
-    const formContainer = document.getElementById("form-container");
-    const form = document.createElement('form');
-    // form.innerHTML = `<input placehold='name' name='name' label='name' type='text' /><br><input placehold='email' name= 'email' type='text' /><br><input value='Add New Interpreter' type='submit' />`
-    const nameLabel = document.createElement('label')
-    nameLabel.innerText = "Name:"
-    const nameInput = document.createElement('input')
-    nameInput.type = 'text'
-    nameInput.name = 'name'
-    const languageLabel = document.createElement('label')
-    languageLabel.innerText = "Language:"
-    const languageInput = document.createElement('input')
-    languageInput.type = 'text'
-    languageInput.name = 'language_id'
-    const cityLabel = document.createElement('label')
-    cityLabel.innerText = "City:"
-    const cityInput = document.createElement('input')
-    cityInput.type = 'text'
-    cityInput.name = 'city_id'
-    const intSubmit = document.createElement('input')
-    intSubmit.type = 'submit'
-    intSubmit.value = 'Create New Interpreter'
-    form.append(nameLabel, nameInput, languageLabel, languageInput, cityLabel, cityInput, intSubmit)
-    formContainer.append(form)
-    form.addEventListener("submit", handleCreateInterpreter)
-}
+// function addCreateForm() {
+//     const formContainer = document.getElementById("form-container");
+//     const form = document.createElement('form');
+//     // form.innerHTML = `<input placehold='name' name='name' label='name' type='text' /><br><input placehold='email' name= 'email' type='text' /><br><input value='Add New Interpreter' type='submit' />`
+//     const nameLabel = document.createElement('label')
+//     nameLabel.innerText = "Name:"
+//     const nameInput = document.createElement('input')
+//     nameInput.type = 'text'
+//     nameInput.name = 'name'
+//     const languageLabel = document.createElement('label')
+//     languageLabel.innerText = "Language:"
+//     const languageInput = document.createElement('input')
+//     languageInput.type = 'text'
+//     languageInput.name = 'language_id'
+//     const cityLabel = document.createElement('label')
+//     cityLabel.innerText = "City:"
+//     const cityInput = document.createElement('input')
+//     cityInput.type = 'text'
+//     cityInput.name = 'city_id'
+//     const intSubmit = document.createElement('input')
+//     intSubmit.type = 'submit'
+//     intSubmit.value = 'Create New Interpreter'
+//     form.append(nameLabel, nameInput, languageLabel, languageInput, cityLabel, cityInput, intSubmit)
+//     formContainer.append(form)
+//     form.addEventListener("submit", handleCreateInterpreter)
+// }
 
-function showCreateForm() {
-    btn = document.getElementById('new-int-btn')
-    btn.addEventListener('click', addCreateForm)
-}
+// function showCreateForm() {
+//     btn = document.getElementById('new-int-btn')
+//     btn.addEventListener('click', addCreateForm)
+// }
 
 function handleCreateInterpreter(e) {
     e.preventDefault()
@@ -96,6 +97,7 @@ function listenforDelete() {
 
 function handleDelete(e) {
     const id = e.target.previousElementSibling.dataset.id
+    const li = e.target.previousElementSibling
         if (e.target.dataset.action == 'delete'){
             // debugger
             // delete this interpreter from backend
@@ -116,7 +118,14 @@ function handleDelete(e) {
                 // console.log(data)
                 // debugger
                 if (data.message === "Successfully deleted"){
-                    e.target.previousElementSibling.dataset.id.remove
+                    console.log(li)
+                    li.remove()
+                    // const toRemove = document.getElementById(`data-id=${id}`)
+                    // console.log(toRemove)
+                    // e.target.previousElementSibling.dataset.remove()
+                    // const interpreterContainer = document.getElementById("interpreters-container")
+                    // interpreterContainer.getInterpreters
+
                 } else {
                     alert(data.message)
                 }

3b2a89a4469eb7e6b74ff5fd1c0215aaeaebe5a9
JillKlatt <klatt.jill@gmail.com>
2021-05-24 10:14:54 -0400

    Add .all method and addlangtodom method

--- front-end/src/components/language.js
+++ front-end/src/components/language.js
@@ -1,6 +1,20 @@
 class Language {
+    static all = []
     constructor({id, name}){
         this.id = id
         this.name = name
+    Language.all.push(this)
+
     }
+    
+addLanguageToDom(){
+    const languageDropDown = document.getElementById("language-dropdown")
+    const languageInput = document.createElement('option')
+    // debugger
+    languageInput.value = this.id
+    languageInput.innerText = this.name
+    // languageInput.value = language.all.forEach(language => language.name)
+    // languageInput.innerText = language.all.forEach(language => language.id)
+    languageDropDown.append(languageInput)
+}
 }

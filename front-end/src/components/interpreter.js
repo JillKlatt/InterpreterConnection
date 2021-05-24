f3a97ecb063d1a3022ce6995228213f5dd11fa63
JillKlatt <klatt.jill@gmail.com>
2021-05-24 16:04:29 -0400

    Add dataset to interpreter objects

--- front-end/src/components/interpreter.js
+++ front-end/src/components/interpreter.js
@@ -16,11 +16,19 @@ class Interpreter {
         // debugger
         const interpreterCity = City.all.find(city => city.id = `${this.cityId}`).name
         const interpreterLanguage = Language.all.find(language => language.id = `${this.languageId}`).name
+        const dataId = this.id
         const deleteIntBtn = document.createElement('button')
         deleteIntBtn.innerText = "X"
         deleteIntBtn.action = 'delete'
-        interpreterContainer.innerHTML += `<li>${this.name} - ${interpreterLanguage}, in ${interpreterCity}</li><button data-action='delete'>X</button><button data-action='favorite'>&#9829;</button>`
+        //interpreterContainer.innerHTML += this.renderInterpreter()
+
+        interpreterContainer.innerHTML += `<li data-set=${dataId}>${this.name} - ${interpreterLanguage}, in ${interpreterCity}</li><button data-action='delete'>X</button><button data-action='favorite'>&#9829;</button>`
 //*  *//
 // ${deleteIntBtn}
     }
+
+    // renderInterpreter(){
+    //     debugger
+    //     return `<li>${this.name} - ${interpreterLanguage}, in ${interpreterCity}</li><button data-action='delete'>X</button><button data-action='favorite'>&#9829;</button>`
+    // }
 }

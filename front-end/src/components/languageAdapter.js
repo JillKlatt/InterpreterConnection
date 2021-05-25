2ed738a74ad5c38d1350cc188859101fefb3f494
JillKlatt <klatt.jill@gmail.com>
2021-05-25 10:20:18 -0400

    Clean up Lang Adapter file

--- front-end/src/components/languageAdapter.js
+++ front-end/src/components/languageAdapter.js
@@ -21,24 +21,3 @@ class LanguageAdapter {
 }
 
 }
-
-// constructor(baseURL){
-//     this.baseCityURL = `${baseURL}/api/v1/cities`
-//     this.cityDropDown = document.getElementById('city-dropdown')
-// }
-
-// getCities(){
-// fetch(this.baseCityURL)
-// .then(resp => resp.json())
-// .then(data => {
-//     data.forEach(function(city){
-//         const c = new City(city)
-//         // debugger
-//         c.addCityToDom()
-
-//         // cityContainer.innerHTML += `<li>${city.name}</li>`
-//     })
-// })
-// // .then(data => console.log(data))
-// .catch(error => console.error(error))
-// }

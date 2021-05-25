c60fdf9b8f68c69007aa9e3fd90ca197194ecb86
JillKlatt <klatt.jill@gmail.com>
2021-05-25 13:15:23 -0400

    Move handleCreateInterpreter to int form, move it back

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -9,8 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
     languageAdapter.getLanguages(); 
     
     interpreterAdapter.getInterpreters();
-// debugger
-    // interpreterForm.addCreateForm();
     interpreterForm.showCreateForm();
     interpreterAdapter.listenforClick();
 })
@@ -21,7 +19,6 @@ function handleCreateInterpreter(e) {
     // debugger
     e.preventDefault()
     // debugger
-    // let nameInput = e.target.children[1].value
     let nameInput = document.getElementById('name-input').value
     let languageInput = e.target.children[3].value
     let cityInput = e.target.children[5].value
@@ -50,68 +47,8 @@ function handleCreateInterpreter(e) {
             const i = new Interpreter(data)
             // debugger
             i.addIntToDom()
-        // } else {
-        //     alert(data.errors)
-        // }
-        // debugger
         document.getElementById('name-input').value = ""
-        // nameInput = ""
-        // languageInput = ""
-        // cityInput = ""
-        // debugger
     })
     .catch(err => console.error("Catch Error:", err))
 }
-    // debugger
-
-
-// function addIntToDom(data) {
-//     const interpreterContainer = document.getElementById('interpreters-container');
-//     interpreterContainer.innerHTML += `<li>${data.name} -${data.language.name}, in ${data.city.name}</li>`
-// }
-
-// function listenforDelete() {
-//     const interpreterContainer = document.getElementById("interpreters-container")
-//     interpreterContainer.addEventListener('click', handleDelete)
-// }
-
-// function handleDelete(e) {
-//     const id = e.target.previousElementSibling.dataset.id
-//     const li = e.target.previousElementSibling
-//         if (e.target.dataset.action == 'delete'){
-//             // debugger
-//             // delete this interpreter from backend
-//             fetch(`http://localhost:3000/api/v1/interpreters/${id}`, {
-//             // console.log(id)
-//             method: "DELETE",
-//             })
-//             .then(resp => {
-     
-//                 console.log(resp)
-//                 return resp.json()
-//             })
-//             .then(data => {
-//                 // debugger
-//                 // if (data.status === 204) {
-//                     // e.target.previousElementSibling.dataset.remove
-//                 // }
-//                 // console.log(data)
-//                 // debugger
-//                 if (data.message === "Successfully deleted"){
-//                     console.log(li)
-//                     li.remove()
-//                     // const toRemove = document.getElementById(`data-id=${id}`)
-//                     // console.log(toRemove)
-//                     // e.target.previousElementSibling.dataset.remove()
-//                     // const interpreterContainer = document.getElementById("interpreters-container")
-//                     // interpreterContainer.getInterpreters
-
-//                 } else {
-//                     alert(data.message)
-//                 }
-
-//             })
     
-//             // .catch(err => console.error(err))
-//     }
-// }

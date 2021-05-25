d65af792640ecbe0d73acf11744acf0f065ecf82
JillKlatt <klatt.jill@gmail.com>
2021-05-25 12:17:23 -0400

    Create form elements in individual city class

--- front-end/src/components/city.js
+++ front-end/src/components/city.js
@@ -1,3 +1,7 @@
+const cityDropdown = document.createElement("select")
+cityDropdown.id = "city-dropdown"
+cityDropdown.name = "city"
+
 class City {
     static all = []
     constructor({id, name}){
@@ -6,16 +10,30 @@ class City {
         
         City.all.push(this)
     }
+
+    // fillCities(){
+    //     debugger
+    //     City.all.forEach(city => city.addCityToDom())
+    // }
+
     addCityToDom(){
-        const cityDropDown = document.getElementById("city-dropdown")
+        // const cityDropDown = document.getElementById("city-dropdown")
         const cityInput = document.createElement('option')
+        // const cityDropdown = document.getElementById("city-dropdown")
+
+
+        // debugger
+
+        citAdapter.getCities()
         // debugger
         cityInput.value = this.id
         cityInput.innerText = this.name
         // cityInput.value = City.all.forEach(city => city.name)
         // cityInput.innerText = City.all.forEach(city => city.id)
         // debugger
-        cityDropDown.append(cityInput)
+        const form = document.getElementById("form-container")
+        form.append(cityDropdown)
+        cityDropdown.append(cityInput)
     }
 
 }

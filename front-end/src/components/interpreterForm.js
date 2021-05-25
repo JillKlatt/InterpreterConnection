6f777e8fbe8657953773247e850f5f3de19dd700
JillKlatt <klatt.jill@gmail.com>
2021-05-25 13:15:39 -0400

    Move handleCreateInterpreter to int form, move it back

--- front-end/src/components/interpreterForm.js
+++ front-end/src/components/interpreterForm.js
@@ -176,4 +176,41 @@ class InterpreterForm {
     // handleFavorite(e) {
     //     console.log(e.target)
     // }
+
+    // handleCreateInterpreter(e) {
+    //     // debugger
+    //     e.preventDefault()
+    //     // debugger
+    //     let nameInput = document.getElementById('name-input').value
+    //     let languageInput = e.target.children[3].value
+    //     let cityInput = e.target.children[5].value
+    
+    
+    //     // Acquired the input of our user, now send it to the backend
+    //     fetch("http://localhost:3000/api/v1/interpreters", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Accept": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             name: nameInput,
+    //             language_id: languageInput,
+    //             city_id: cityInput
+    //         })   
+    //     })
+    //     .then(resp => resp.json())
+        
+    //     .then(data => {
+    //         // debugger
+    //         console.log(data)
+    //         // debugger
+    //         // if (data.status === 201){
+    //             const i = new Interpreter(data)
+    //             // debugger
+    //             i.addIntToDom()
+    //         document.getElementById('name-input').value = ""
+    //     })
+    //     .catch(err => console.error("Catch Error:", err))
+    // }
 }

8768d6f6aca9872595539e82f41f1b997b334757
JillKlatt <klatt.jill@gmail.com>
2021-05-25 12:30:03 -0400

    Remove 12 debuggers to prepare to copy to languages

--- front-end/src/components/city.js
+++ front-end/src/components/city.js
@@ -11,31 +11,16 @@ class City {
         City.all.push(this)
     }
 
-    // fillCities(){
-    //     debugger
-    //     City.all.forEach(city => city.addCityToDom())
-    // }
 
     addCityToDom(){
-        // const cityDropDown = document.getElementById("city-dropdown")
+
         const cityInput = document.createElement('option')
-        // const cityDropdown = document.getElementById("city-dropdown")
-
-
-        // debugger
-
         cityAdapter.getCities()
-        // debugger
         cityInput.value = this.id
         cityInput.innerText = this.name
-        // cityInput.value = City.all.forEach(city => city.name)
-        // cityInput.innerText = City.all.forEach(city => city.id)
-        //debugger
         const form = document.getElementById("int-form")
-        // debugger
         form.append(cityDropdown)
         cityDropdown.append(cityInput)
-        // debugger
     }
 
 }

0c93d627307f7c46e5103531db18316c793c4952
JillKlatt <klatt.jill@gmail.com>
2021-05-27 18:56:50 -0400

    Remove old form from interpreter form to store info for popout

--- front-end/src/components/interpreterForm.js
+++ front-end/src/components/interpreterForm.js
@@ -52,10 +52,7 @@ class InterpreterForm {
 
         const cityLabel = document.createElement('label')
         cityLabel.innerText = "City:"
-        // const cityInput = document.createElement('input')
-        // cityInput.type = 'text'
-        // cityInput.name = 'city_id'
-        // const cityDropdown = document.getElementById("city-dropdown")
+
         const cityDropdown = document.createElement("select")
         cityDropdown.id = "city-dropdown"
 
@@ -63,10 +60,9 @@ class InterpreterForm {
         const intSubmit = document.createElement('input')
         intSubmit.type = 'submit'
         intSubmit.value = 'Create New Interpreter'
-        // cityDropdown.append(cityInput)
-        form.append(nameLabel, nameInput, languageLabel ) //cityDropdown,languageDropdown, 
-        // allLang.forEach(language => language.addLanguageToDom())
-        // City.all.forEach(city => city.addCityToDom())
+ 
+        form.append(nameLabel, nameInput, languageLabel)
+
         formContainer.append(form)
 
         // debugger
@@ -88,9 +84,7 @@ class InterpreterForm {
         const btn = e.target
         const likeBtn = e.target.nextElementSibling
         const action = e.target.dataset.action
-        // console.log(e.target)
             switch(action){ 
-           // if (e.target.dataset.action == 'delete'){
                 case "delete": 
                 console.log("deleting")
                     // debugger
@@ -117,12 +111,7 @@ class InterpreterForm {
                             li.remove()
                             btn.remove()
                             likeBtn.remove()
-                            // const toRemove = document.getElementById(`data-id=${id}`)
-                            // console.log(toRemove)
-                            // e.target.previousElementSibling.dataset.remove()
-                            // const interpreterContainer = document.getElementById("interpreters-container")
-                            // interpreterContainer.getInterpreters
-        
+          
                         } else {
                             alert(data.message)
                         }})
@@ -168,12 +157,7 @@ class InterpreterForm {
 
                     const addId = parseInt(e.target.previousElementSibling.previousElementSibling.dataset.set)
 
-                    // const intName = Interpreter.all.find(int => int.id === addId).name
                     const intName = e.target.previousElementSibling.previousElementSibling.children[0].innerText
-                    
-
-                    // Add conditional to see if favorite exists? Or I guess just toggle between favorite and not?
-                    // debugger
 
                     const favoritesContainer = document.getElementById("favorites-container")
 
@@ -190,23 +174,8 @@ class InterpreterForm {
                     showMoreBtn.setAttribute("data-bs-target", "#show-modal")
                     let infoDiv = document.createElement("div")
                     infoDiv.id = "info-div"
-                    // const email = document.createElement("li")
-                    // email.innerText = int.email
-                    // const phone = document.createElement("li")
-                    // phone.innerText = int.phone
-                    // const notes = document.createElement("li")
-                    // notes.innerText = int.notes
-                    // infoDiv.append(email, phone, notes)
-                    // infoDiv.classList.add("hidden")
                     ul.append(infoDiv, showMoreBtn)
-                    favoritesContainer.append(ul)
-                    // console.log(`Adding ${intName} to favorites`)
-                    // alert(`Added ${intName} to favorites`)
-                    //     // debugger
-                    // favorites.push(addId)
-                    // // debugger                    
-                    // localStorage.setItem("favorites", JSON.stringify(favorites))
-              
+                    favoritesContainer.append(ul)            
 
                     break;   
                 

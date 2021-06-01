250290aaea73021d5372fe729bdc24f702b4dc3c
JillKlatt <klatt.jill@gmail.com>
2021-06-01 12:51:49 -0400

    Add seed data for cloning

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -6,15 +6,60 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+Language.create(name: "Alienese")
+Language.create(name: "American Sign Language")
+Language.create(name: "Arabic")
+Language.create(name: "Dothraki")
 Language.create(name: "English")
 Language.create(name: "French")
+Language.create(name: "German")
+Language.create(name: "Hindi")
+Language.create(name: "Japanese")
+Language.create(name: "Klingon")
+Language.create(name: "Mandarin")
+Language.create(name: "Portuguese")
+Language.create(name: "Russian")
 Language.create(name: "Spanish")
+# Total Languages: 14
 
-City.create(name: "Cleveland")
+
+
+
+
 City.create(name: "Chicago")
+City.create(name: "Cleveland")
+City.create(name: "Columbus")
+City.create(name: "Denver")
+City.create(name: "Dictionopolis")
+City.create(name: "Houston")
+City.create(name: "Kansas City")
+City.create(name: "Las Vegas")
+City.create(name: "Los Angeles")
 City.create(name: "New York")
+City.create(name: "Phandelver")
+City.create(name: "Phoenix")
+City.create(name: "San Antonio")
+City.create(name: "San Diego")
+City.create(name: "Westeros")
+# Total cities: 16
 
-Interpreter.create(name: "Jill", email: "jill@gmail.com", phone: "123-456-7891", notes: "Nice Gal", city_id: 1, language_id: 1)
-Interpreter.create(name: "Maggie", email: "maggie@gmail.com", phone: "123-456-7891", notes: "Old Lady", city_id: 2, language_id: 3)
-Interpreter.create(name: "Thunder", email: "thunder@gmail.com", phone: "123-456-7891", notes: "Grumpy Man", city_id: 3, language_id: 2)
+
+
+
+Interpreter.create({
+    name: "Jill Klatt", 
+    email: "jill@gmail.com", 
+    phone: "123-456-7891", 
+    notes: "Nice Gal", 
+    city_id: 1, 
+    language_id: 1
+})
+5.times do
+    Interpreter.create({
+        "name" => Faker::Name.name, 
+        "email" => Faker::Internet.email, 
+        "phone" => Faker::PhoneNumber.cell_phone, 
+        "notes" => Faker::Demographic.educational_attainment, 
+        "city_id" => Faker::Number.between(from: 1, to: 16), 
+        "language_id" => Faker::Number.between(from: 1, to: 14)
+    })

537db750d9c972e32fc97d394ecd7e62a0a75904
JillKlatt <klatt.jill@gmail.com>
2021-06-02 15:34:08 -0400

    Remove green text

--- front-end/src/components/interpreterAdapter.js
+++ front-end/src/components/interpreterAdapter.js
@@ -11,15 +11,11 @@ class InterpreterAdapter {
     .then(resp => resp.json())
     .then(data => {
         data.forEach(interpreter => {
-            // debugger
             const i = new Interpreter(interpreter)
 
             i.addIntToDom()
-            //interpreterContainer.innerHTML += `<li data-id="${interpreter.id}">${interpreter.name} -${interpreter.language.name}, in ${interpreter.city.name}</li><button data-action='delete'>X</button><button data-action='favorite'>&#9829;</button>`
-            // console.log(interpreter)
         })
     })
-    // .then(data => console.log(data))
     .catch(error => console.error(error))
 }
 
@@ -28,57 +24,5 @@ class InterpreterAdapter {
         interpreterContainer.addEventListener('click', interpreterForm.handleInterpreterClick)
     }
     
-    // handleDelete(e) {
-    //     const id = e.target.previousElementSibling.dataset.id
-    //     const li = e.target.previousElementSibling
-    //     debugger
-    //     const btn = e.target
-    //     console.log(e.target)
-    //         if (e.target.dataset.action == 'delete'){
-    //             // debugger
-    //             // delete this interpreter from backend
-    //             fetch(`http://localhost:3000/api/v1/interpreters/${id}`, {
-    //             // console.log(id)
-    //             method: "DELETE",
-    //             })
-    //             .then(resp => {
-         
-    //                 console.log(resp)
-    //                 return resp.json()
-    //             })
-    //             .then(data => {
-    //                 // debugger
-    //                 // if (data.status === 204) {
-    //                     // e.target.previousElementSibling.dataset.remove
-    //                 // }
-    //                 // console.log(data)
-    //                 // debugger
-    //                 if (data.message === "Successfully deleted"){
-    //                     console.log(li)
-    //                     li.remove()
-    //                     // const toRemove = document.getElementById(`data-id=${id}`)
-    //                     // console.log(toRemove)
-    //                     // e.target.previousElementSibling.dataset.remove()
-    //                     // const interpreterContainer = document.getElementById("interpreters-container")
-    //                     // interpreterContainer.getInterpreters
-    
-    //                 } else {
-    //                     alert(data.message)
-    //                 }
-    
-    //             })
-        
-    //             // .catch(err => console.error(err))
-    //     }
-    // }
-
-    
-    // listenforDelete() {
-    //     interpreterContainer.addEventListener('click', interpreterForm.handleDelete)
-    // }
-
-    // handleFavorite(e) {
-    //     console.log(e.target)
-    // }
 }
 

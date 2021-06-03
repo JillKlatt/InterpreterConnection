7f41495c1cb9e434042d0087e45efff708341c9a
JillKlatt <klatt.jill@gmail.com>
2021-06-03 15:23:47 -0400

    Add some methods to faves adapater

--- front-end/src/components/favoritesAdapter.js
+++ front-end/src/components/favoritesAdapter.js
@@ -4,18 +4,21 @@ class FavoritesAdapter {
         switch (displayFavorites) {
             case true:
                 console.log("Showing Favorites")
-                favsButton.innerText = "Hide My Faves"
-                populateFavs(favsArray)
+                favesButton.innerText = "Hide My Faves"
+                populateFaves(favesArray)
                 favoriteInterpContainer.classList.remove("hidden")
                 displayFavorites = false
                 break;
             case false:
-                favsButton.innerText = "Show My Faves"
+                favesButton.innerText = "Show My Faves"
                 favoriteInterpContainer.classList.add("hidden")
                 displayFavorites = true
                 break;
         }
     }
-
-
+    listenforFaveClick() {
+        const favesContainer = document.getElementById("favorites-container")
+            //const showMoreBtn = document.getElementById("show-more-button")
+        favesContainer.addEventListener("click", displayPopUp)
+    }
 }
